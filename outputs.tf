59f24c28fee377f65bb0054cce6aeeaed82f99f8
ishara <ithapa@clarku.edu>
2025-11-02 02:39:24 -0500

    Commented out EC2 outputs for CI-safe validation

--- outputs.tf
+++ outputs.tf
@@ -13,12 +13,12 @@ output "security_group_id" {
   value       = aws_security_group.web.id
 }
 
-output "instance_id" {
-  description = "EC2 instance id"
-  value       = aws_instance.web.id
-}
+#output "instance_id" {
+ # description = "EC2 instance id"
+ # value       = aws_instance.web.id
+#}
 
-output "instance_public_ip" {
-  description = "Public IP of the instance"
-  value       = aws_instance.web.public_ip
-}
+#output "instance_public_ip" {
+ # description = "Public IP of the instance"
+ # value       = aws_instance.web.public_ip
+#}
