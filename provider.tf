72b0ada9ab5559cd6ca8628c9f934c458513bbc7
ishara <ithapa@clarku.edu>
2025-11-02 03:05:22 -0500

    Fix Terraform formatting and provider.tf structure

--- provider.tf
+++ provider.tf
@@ -10,8 +10,6 @@ terraform {
 
 provider "aws" {
   region                      = "us-east-1"
-
- 
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
