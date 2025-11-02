b04450900e9f57bed9a31d525b3091c99c08d9dd
ishara <ithapa@clarku.edu>
2025-11-02 03:00:00 -0500

    Fix provider.tf for CI-safe AWS skip auth

--- provider.tf
+++ provider.tf
@@ -10,6 +10,8 @@ terraform {
 
 provider "aws" {
   region                      = "us-east-1"
+
+ 
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
